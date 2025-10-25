1192750e334e531c4dcd268b5e655027dedc8975
chanakaekanayaka <chanakaekanayaka15@gmail.com>
2025-10-26 04:06:55 +0530

    setup project and folder structure 🚀

---
+++ client/src/pages/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <div>
+
+        <h1>Layout Page</h1>
+      
+    </div>
+  )
+}
+
+export default Layout
---
+++ client/src/pages/NextGen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NextGen = () => {
+  return (
+    <div>
+
+        <h1>NextGen page</h1>
+      
+    </div>
+  )
+}
+
+export default NextGen
