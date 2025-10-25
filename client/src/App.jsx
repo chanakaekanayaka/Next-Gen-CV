f8f5ba20b36067a67ee686f740b17686da5d9f47
chanakaekanayaka <chanakaekanayaka15@gmail.com>
2025-10-26 04:27:57 +0530

    set up Routes ✨

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import NextGen from './pages/NextGen'
+import Preview from './pages/Preview'
+import Login from './pages/Login'
 
 const App = () => {
   return (
-    <div>
+    <>
+      <Routes>
+        <Route path='/' element={<Homepage></Homepage>}/>
+
+        <Route path='app' element={<Layout></Layout>}>
+          <Route index element={<Dashboard></Dashboard>}></Route>
+          <Route path='builder/:resumeId' element={<NextGen></NextGen>}></Route>
+        </Route>
+
+        <Route path='view/:resumeId' element={<Preview></Preview>}></Route>
+        <Route path='login' element={<Login></Login>}></Route>
+
+      </Routes>
       
-    </div>
+    </>
   )
 }
 
