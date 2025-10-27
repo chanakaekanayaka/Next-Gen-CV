7fb5dc3b11a01dc234e6d767df97958b2acee5f4
chanakaekanayaka <chanakaekanayaka15@gmail.com>
2025-10-28 03:50:23 +0530

    Home page done✨

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
 
@@ -16,19 +17,19 @@ const Hero = () => {
                     </a>
 
                     <div className="hidden md:flex items-center gap-8 transition duration-500 text-slate-800">
-                        <a href="#" className="hover:text-indigo-600 transition">Home</a>
-                        <a href="#features" className="hover:text-indigo-600 transition">Features</a>
-                        <a href="#testimonials" className="hover:text-indigo-600 transition">Testimonials</a>
-                        <a href="#cta" className="hover:text-indigo-600 transition">Contact</a>
+                        <a href="#" className="hover:text-indigo-600 transition ">Home</a>
+                        <a href="#features" className="hover:text-indigo-600 transition ">Features</a>
+                        <a href="#testimonials" className="hover:text-indigo-600 transition ">Testimonials</a>
+                        <a href="#cta" className="hover:text-indigo-600 transition ">Contact</a>
                     </div>
 
                     <div className="flex gap-2">
-                        <a href="" className="hidden md:block px-6 py-2 bg-indigo-500 hover:bg-indigo-700 active:scale-95 transition-all rounded-full text-white">
+                        <Link to={'/app?state=register'} className="hidden md:block px-6 py-2 bg-indigo-500 hover:bg-indigo-700 active:scale-95 transition-all rounded-full text-white">
                             Get started
-                        </a>
-                        <a href="" className="hidden md:block px-6 py-2 border active:scale-95 hover:bg-slate-50 transition-all rounded-full text-slate-700 hover:text-slate-900" >
+                        </Link>
+                        <Link to={'/app?state=login'} className="hidden md:block px-6 py-2 border active:scale-95 hover:bg-slate-50 transition-all rounded-full text-slate-700 hover:text-slate-900" >
                             Login
-                        </a>
+                        </Link>
                     </div>
 
                     <button onClick={() => setMenuOpen(true)} className="md:hidden active:scale-90 transition" >
@@ -38,16 +39,8 @@ const Hero = () => {
                     </button>
                 </nav>
 
-                {/* Mobile Menu */}
-                <div className={`fixed inset-0 z-[100] bg-black/40 text-black backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-300 ${menuOpen ? "translate-x-0" : "-translate-x-full"}`} >
-                    <a href="/" className="text-white">Home</a>
-                    <a href="/products" className="text-white">Products</a>
-                    <a href="/stories" className="text-white">Stories</a>
-                    <a href="/pricing" className="text-white">Pricing</a>
-                    <button onClick={() => setMenuOpen(false)} className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-indigo-600 hover:bg-indigo-700 transition text-white rounded-md flex" >
-                        X
-                    </button>
-                </div>
+               
+                
 
                 {/* Hero Section */}
                 <div className="relative flex flex-col items-center justify-center text-sm px-4 md:px-16 lg:px-24 xl:px-40 text-black">
@@ -84,14 +77,11 @@ const Hero = () => {
 
                     {/* CTA Buttons */}
                     <div className="flex items-center gap-4 ">
-                        <a href='/' className="bg-indigo-500 hover:bg-indigo-600 text-white rounded-full px-9 h-12 m-1 ring-offset-2 ring-1 ring-indigo-400 flex items-center transition-colors">
-                            Get started
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-right ml-1 size-4" aria-hidden="true"><path d="M5 12h14"></path><path d="m12 5 7 7-7 7"></path></svg>
-                        </a>
-                        <button className="flex items-center gap-2 border border-slate-400 hover:bg-indigo-50 transition rounded-full px-7 h-12 text-slate-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-video size-5" aria-hidden="true"><path d="m16 13 5.223 3.482a.5.5 0 0 0 .777-.416V7.87a.5.5 0 0 0-.752-.432L16 10.5"></path><rect x="2" y="6" width="14" height="12" rx="2"></rect></svg>
-                            <span>Try demo</span>
-                        </button>
+                        <Link to={'/app'} className="bg-indigo-500 hover:bg-indigo-600 text-white rounded-full px-9 h-12 m-1 ring-offset-2  flex items-center transition-colors ">
+                            <span className=''>Get started</span>
+                            <svg   xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-right ml-1 size-4" aria-hidden="true"><path d="M5 12h14"></path><path d="m12 5 7 7-7 7"></path></svg>
+                        </Link>
+                       
                     </div>
 
                    
