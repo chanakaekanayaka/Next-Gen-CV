d58596efbb7dce75a35ca9b7094581b6b290c79c
chanakaekanayaka <chanakaekanayaka15@gmail.com>
2025-12-13 23:46:00 +0530

    Share button, visibility button and download button completed✨

--- client/src/pages/NextGen.jsx
+++ client/src/pages/NextGen.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { dummyResumeData } from '../assets/assets'
-import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkle, Sparkles, User } from 'lucide-react'
+import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, DownloadIcon, EyeIcon, EyeOff, FileText, FolderIcon, GraduationCap, Share2Icon, Sparkle, Sparkles, User } from 'lucide-react'
 import PersonalInfoForm from '../components/PersonalInfoForm'
 import ResumePreview from '../components/ResumePreview'
 import TemplateSelector from '../components/TemplateSelector'
@@ -26,7 +26,7 @@ const NextGen = () => {
     skills:[],
     template:"classic",
     accent_color:"#3B82F6",
-    pblic: false,
+    public: false,
   })
 
   
@@ -55,9 +55,33 @@ const NextGen = () => {
 
   const activeSection = sections[activeSectionIndex]
 
-    useEffect(()=>{
+  useEffect(()=>{
     loadExistinfResume()
   },[])
+
+
+  const changeResumeVisibility = async ()=>{
+    setResumeData({...resumeData , public: !resumeData.public})
+  }
+
+  const handleShare = () =>{
+    const frontendUrl = window.location.href.split('/app/')[0];
+    const resumeUrl = frontendUrl + '/view/' +resumeId
+
+    if(navigator.share){
+      navigator.share({url:resumeUrl , text: "My Resume"})
+    }else{
+      alert('Share not supported on this browser.')
+    }
+  }
+
+  const downloadResume = ()=>{
+    window.print();
+  }
+
+
+
+
 
 
   return (
@@ -162,10 +186,37 @@ const NextGen = () => {
 
             {/*Right panel-preview*/}
              <div className='lg:col-span-7 max-lg:mt-6'>
+              <div className='relative w-full'>
+                
+                <div className='absolute bottom-3 left-0 right-0 flex items-center justify-end gap-2'>
+                  {/*share button*/}
+                  {resumeData.public && (
+                    <button onClick={handleShare} className='flex items-center p-2 px-4 gap-2 text-xs
+                    bg-gradient-to-br from-blue-100 to-blue-200 text-blue-600
+                    rounded-lg ring-blue-300 hover:ring transition-colors'>
+                      <Share2Icon className='size-4'/>
+                        Share
+                    </button>
+                  )}
+                  {/*Visibility  button*/}
+                  <button onClick={changeResumeVisibility} className='flex items-center p-2 px-4 text-xs bg-gradient-to-br
+                  from-purple-200 to-purple-500 ring-purple-400 rounded-lg hover:ring transition-colors '>
+                    {resumeData.public ? <EyeIcon className='size-4'/> :
+                     <EyeOff className='size-4'/>}
+                     {resumeData.public ? 'public' : 'private'}
+                  </button>
+                  {/*Download button*/}
+                  <button onClick={downloadResume} className='flex items-center gap-2 px-6 py-2 text-xs bg-gradient-to-br
+                  from-green-200 to-green-400 text-green-600 rounded-lg ring-green-300 hover:ring
+                  transition-colors'>
+                      <DownloadIcon className='size-4'/>Download
+                  </button>
 
-              <div>
-                {/*buttons*/}
+                </div>
+
               </div>
+
+              
 
               {/*reume preview*/}
               <ResumePreview data={resumeData} template={resumeData.template} accentColor={resumeData.accent_color}/>
