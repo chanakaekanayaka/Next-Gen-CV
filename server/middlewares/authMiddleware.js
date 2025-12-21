c4c50c3fd0f26ad34d688382376a10f224702314
chanakaekanayaka <chanakaekanayaka15@gmail.com>
2025-12-21 14:31:38 +0530

    user controller and auth middleware completed✨

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+
+const protect = async (req , res , next)=>{
+    const token = req.headers.authorization;
+
+    if(!token){
+        return res.status(401).json({message:'Unothorized'});
+    }
+
+    try {
+        const decode = jwt.verify(token,process.env.JWT_SECRET)
+        req.userId = decode.userId;
+        next();
+        
+    } catch (error) {
+        return res.status(401).json({message:'Unauthorized'})
+        
+    }
+}
+
+export default protect;
