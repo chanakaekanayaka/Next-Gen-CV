630e2492a994d85d1c210754822c41ee9d210552
chanakaekanayaka <chanakaekanayaka15@gmail.com>
2025-12-21 10:28:08 +0530

    Database connection completed✨

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./configs/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Database connection
+await connectDB()
+
 app.use(express.json())
 app.use(cors())
+
 
 app.get("/" , (req,res)=>res.send("Server is live.."))
 
